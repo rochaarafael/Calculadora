115cb6471c8c545ad4bd51d89f1e822eea0314dc
Meckow <pennharafael@gmail.com>
2021-09-08 14:07:20 -0300

    Finalização

--- script.js
+++ script.js
@@ -22,8 +22,38 @@ function subtrair() {
     } else {
         let n1 = Number(num1.value)
         let n2 = Number(num2.value)
-        let r = n1 + n2 
+        let r = n1 - n2 
         res.innerHTML=`<p>O resultado da subtração é: <br> ${r}</p> `
+    }
+
 }
 
+function dividir() {
+    let num1 = document.getElementById('n1')
+    let num2 = document.getElementById('n2')
+    let res = document.getElementById('res')
+    
+    if (num1.value.length == 0 || num2.value.length == 0 ) {
+        res.innerHTML='Por favor, preencha todos os espaços de números.'
+    } else {
+        let n1 = Number(num1.value)
+        let n2 = Number(num2.value)
+        let r = n1 / n2 
+        res.innerHTML=`<p>O resultado da divisão é: <br> ${r}</p> `
+    }
+}
+
+function multiplicar() {
+    let num1 = document.getElementById('n1')
+    let num2 = document.getElementById('n2')
+    let res = document.getElementById('res')
+    
+    if (num1.value.length == 0 || num2.value.length == 0 ) {
+        res.innerHTML='Por favor, preencha todos os espaços de números.'
+    } else {
+        let n1 = Number(num1.value)
+        let n2 = Number(num2.value)
+        let r = n1 * n2 
+        res.innerHTML=`<p>O resultado da multiplicação é: <br> ${r}</p> `
+    }
 }
