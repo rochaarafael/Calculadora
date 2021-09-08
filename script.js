29a51a9abb9563f95c7a69032a2a627fa127899f
Meckow <pennharafael@gmail.com>
2021-09-08 12:03:50 -0300

    Prática

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function somar() {
-    alert('Hello World!')
+    let num1 = document.getElementById('n1')
+    let num2 = document.getElementById('n2')
+    let res = document.getElementById('res')
+    
+    if (num1.value.length == 0 || num2.value.length == 0 ) {
+        res.innerHTML='Por favor, preencha todos os espaços de números.'
+    } else {
+        let n1 = Number(num1.value)
+        let n2 = Number(num2.value)
+        let r = n1 + n2 
+        res.innerHTML=`<p>O resultado da soma é: <br> ${r}</p> `
+    }
+}
+function subtrair() {
+    let num1 = document.getElementById('n1')
+    let num2 = document.getElementById('n2')
+    let res = document.getElementById('res')
+    
+    if (num1.value.length == 0 || num2.value.length == 0 ) {
+        res.innerHTML='Por favor, preencha todos os espaços de números.'
+    } else {
+        let n1 = Number(num1.value)
+        let n2 = Number(num2.value)
+        let r = n1 + n2 
+        res.innerHTML=`<p>O resultado da subtração é: <br> ${r}</p> `
+}
+
 }
